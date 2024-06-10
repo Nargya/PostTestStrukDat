5edf324e6ab13a62cad4df0c62bba889bcc73bf2
Naufal Alif <161468671+Nargya@users.noreply.github.com>
2024-06-10 14:24:47 +0700

    Add files via upload

---
+++ Soal3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int angka[] = {5, 1, 4, 2, 8};
+    int[i] = {"[a]", "[b]", "[c]","[d]","[e]"};
+    int dtsort, hasilsort;
+    for (int[i] == 1, i < [a], i++ );
+    while (int[i] == 0, i > [b], i++);
+    cout << "\nHasil sort" << endl;
+}
